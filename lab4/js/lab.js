ce064783ad346c1db21b59ee598c9448ff833f75
azelfe <130079866+azelfe@users.noreply.github.com>
2023-04-26 22:32:19 -0700

    java

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock paper sissors psudeocode
+// Author: Taylor Wedaa, Maya Garcia
+// Date: April 2023
+
+//Task 1:
+//Making Guacamole
+//Step 1: Get ingredients; avocado, tomatoes, garlic, onions, lemon, salt
+//Step 2: Get supplies: knife, fork, flat bottom bowl
+//Step 3: Cut all the ingredients
+//Step 4: Mash/mix avocado, tomatoes, garlic, and onions together
+//Step 5: Squeeze lemon and put salt on it
+//Step 6: Serve it
+
+//Task 2:
+//Creating a Rock, Paper, Scissors game:
+//1. Allow two users to choose rock, paper, or scissors
+//2. Accept user input
+//3. Test win or draw condition
 
 // Constants
 
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+// Author: Taylor Wedaa
+// Date: April 26th, 2020
 
 // Functions
+// Define Variables
+  var make = "Toyota";
+  var model = "Prius";
+  var color = "Space Gray";
+  var year = 2014;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//calculate
+ age = 2023 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("color: " + color + "<br>");
+  document.writeln("year: " + year + "<br>");
+  document.writeln("age: " + age + "years<br>");
