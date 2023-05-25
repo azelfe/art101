5f415df2ede3ebe0322e66e9640c7c990526f832
azelfe <130079866+azelfe@users.noreply.github.com>
2023-05-24 17:51:26 -0700

    extra

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -8,15 +8,19 @@ for (var i=1; i<200; i++) {
 	str = "";
   // if a multiple of 3, print fuzz
   if (i % 3 == 0) {
-    str += "Fizz";
+    str += "Fizz!";
   }
   // if a multiple of 5, print buzz
   if (i % 5 == 0) {
-    str += "Buzz";
+    str += "Buzz!";
   }
   // if a multiple of 7, print boom
   if (i % 7 == 0) {
-    str += "Boom";
+    str += "Boom!";
+  }
+   // if a multiple of 7, print boom
+   if (i % 10 == 0) {
+    str += "Bang!";
   }
   // if the string is still empty, set str to number
   if (str == "") {
